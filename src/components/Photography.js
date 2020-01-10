fe6569354c6c7eb2bf111c3632c8b3b1bb412185
Revati Rajabhathor <revatirajabhathor@yahoo.com>
2020-01-09 19:29:36 -0500

    Added boilerplate to Photography component

---
+++ src/components/Photography.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Photography() {
+  return (
+    <div className="wrapper">
+      <section id="contact">
+
+      </section>
+    </div>
+  )
+}
+
+export default Photography
