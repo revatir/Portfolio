b7ef3a6cd0744111847bc4dbca520ddd92cecbf9
Revati Rajabhathor <revatirajabhathor@yahoo.com>
2020-02-09 11:59:31 -0500

    [App.js] Restructured components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 
-//React Router
-import { Route } from 'react-router-dom';
-
 //Custom Components
+import Menu from './components/Menu';
 import Header from './components/Header';
-import Footer from './reactImages/Footer';
+import Footer from './components/Footer';
 import About from './components/About';
-import Contacts from './components/Contacts';
+import Contact from './components/Contact';
 import Projects from './components/Projects';
 import Photography from './components/Photography';
 
@@ -17,16 +15,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Menu />
         <Header />
         <div className="siteContent">
-          <Route path="/About" render={(props) =>
-            <About />} />
-          <Route path="/Projects" render={(props) =>
-            <Projects />} />
-          <Route path="/Photography" render={(props) =>
-            <Photography />} />
-          <Route path="/Contacts" render={(props) =>
-            <Contacts />} />
+          <About />
+          <Projects />
+          <Photography />
+          <Contact />
         </div>
         <Footer />
       </div>
