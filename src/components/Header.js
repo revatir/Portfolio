4abd3ef5c8d25667fe7f2d66617f53f60f7f2290
Revati Rajabhathor <revatirajabhathor@yahoo.com>
2020-02-09 12:01:48 -0500

    [Header.js] - added id for single page redirect

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,12 @@
 import React from 'react';
 
-//React Router
-import { Link } from 'react-router-dom';
-
 function Header() {
   return (
-    <div id="headerWrapper">
+    <div class="header-section-wrapper">
       <header>
-        <Link to="/" id="name"><h1>REVATI RAJABHATHOR</h1></Link>
+        <h1>REVATI RAJABHATHOR</h1>
         <h2 id="subheader">Software Engineer</h2>
       </header>
-      <menu>
-        <ul id="menuList">
-          <Link to="About" className="menuItem"><li>About</li></Link>
-          <Link to="Projects" className="menuItem"><li>Projects</li></Link>
-          <Link to="Photography" className="menuItem"><li>Photography</li></Link>
-          <Link to="Contacts" className="menuItem"><li>Contact</li></Link>
-        </ul>
-      </menu>
     </div>
   )
 }
