c98b0551f69443ac4148246d295c7300db8c5929
Revati Rajabhathor <revatirajabhathor@yahoo.com>
2022-04-27 12:18:09 -0400

    implement smooth scroll

--- src/sections/menu/index.js
+++ src/sections/menu/index.js
@@ -1,7 +1,22 @@
 import React from "react"
+import SmoothScroll from "smooth-scroll"
 import "./index.scss"
 
 const Menu = ({ menuOptions, selected, setSelected }) => {
+  const projects = document.querySelector("#projects")
+  const photography = document.querySelector("#photography")
+  const contact = document.querySelector("#contact")
+
+  const scroll = new SmoothScroll('a[href*="#"]', {
+    easing: "easeInOutQuart",
+    offset: function (anchor) {
+      const { id } = anchor
+      if (id === "projects" || id === "photography" || id === "contact")
+        return 125
+      else return 30
+    },
+  })
+
   return (
     <menu>
       <ul className="menu-list">
